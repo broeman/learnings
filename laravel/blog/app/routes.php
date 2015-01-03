c99a9e493c10289451408c02eea17683511dd05d
Jesper Brodersen <brodersendev@gmail.com>
2015-01-03 23:58:54 +0100

    views, basecontroller fix, and routes with a composer

--- laravel/blog/app/routes.php
+++ laravel/blog/app/routes.php
@@ -11,7 +11,51 @@
 |
 */
 
-Route::get('/', function()
+// Route::get('/', function()
+// {
+// 	return View::make('hello');
+// });
+
+
+// Models
+Route::model('post', 'Post');
+Route::model('comment', 'Comment');
+
+// User routes
+Route::get('/post/{post}/show', ['as' => 'post.show', 'uses' => 'PostController@showPost']);
+Route::post('/post/{post}/comment', ['as' => 'comment.new', 'uses' => 'CommentController@newComment']);
+
+
+Route::group(['prefix' => 'admin', 'before'=>'auth'], function()
 {
-	return View::make('hello');
+	Route::get('dash-board', function()
+	{
+		$layout = View::make('master');
+		$layout->title = 'DashBoard';
+		$layout->main = View::make('dash')
+			->with('content', 'Hi admin, Welcome to Dashboard.');
+		return $layout;
+	});
+
+	Route::get('/post/list', ['as' => 'post.list', 'uses' => 'PostController@listPost']);
+	Route::get('/post/new', ['as' => 'post.new', 'uses' => 'PostController@newPost']);
+	Route::get('/post/{post}/edit', ['as' => 'post.edit', 'uses' => 'PostController@editPost']);
+	Route::get('/post/{post}/delete', ['as' => 'post.delete', 'uses' => 'PostController@deletePost']);
+	Route::get('/comment/list', ['as' => 'comment.list', 'uses' => 'CommentController@listComment']);
+	Route::get('/comment/{comment}/show', ['as' => 'comment.show', 'uses' => 'CommentController@showComment']);
+	Route::get('/comment/{comment}/delete', ['as' => 'comment.delete', 'uses' => 'CommentController@deleteComment']);
+
+	Route::post('/post/save', ['as' => 'post.save', 'uses' => 'PostController@savePost']);
+	Route::post('/post/{post}/update', ['as' => 'post.update', 'uses' => 'PostController@updatePost']);
+	Route::post('/comment/{comment}/update', ['as' => 'comment.update', 'uses' => 'CommentController@updateComment']);
+	
+});
+
+// Home Route
+Route::controller('/', 'BlogController');
+
+// Composing views
+View::composer('sidebar', function($view)
+{
+	$view->recentPosts = Post::orderBy('id', 'desc')->take(5)->get();
 });
